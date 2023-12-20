c01c7c44e475f28c44532dccbdf3906c2bd7395f
Matthew Lim <matthewlim230505@gmail.com>
2023-12-20 19:04:59 +0800

    Fix mutation observer to not interfere with Bing Rewards. Changed method for setting the maxlength of search bars.

--- bing-scripts/increase-word-limit.js
+++ bing-scripts/increase-word-limit.js
@@ -3,30 +3,32 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.bing.com/*
 // @license     MIT
-// @version     6.0
+// @version     7.0
 // @author      Reddiepoint
 // @description Increases the word limit for Bing Search and Bing Chat to 1000000.
 // ==/UserScript==
 
 
-const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
         if (mutation.addedNodes.length > 0) {
-            const element = document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container > div > div.input-row > cib-text-input").shadowRoot.querySelector("#searchbox");
+            const element = document.querySelector("#b_sydConvCont > cib-serp")?.shadowRoot
+                ?.querySelector("#cib-action-bar-main")?.shadowRoot
+                ?.querySelector("div > div.main-container > div > div.input-row > cib-text-input")?.shadowRoot
+                ?.querySelector("#searchbox");
+
             if (element) {
                 element.setAttribute("maxlength", "1000000");
                 observer.disconnect();
+                break;
             }
         }
-    });
+    }
 });
 
+observer.observe(document.body, { childList: true, subtree: true });
 
-observer.observe(document.body, {childList: true, subtree: true});
 
-
-Array.from(document.getElementsByTagName('*')).forEach(function(element) {
-    element.addEventListener('focus', function() {
-        this.setAttribute('maxlength', '1000000');
-    });
+Array.from(document.getElementsByName("q")).forEach(function(element) {
+    element.setAttribute("maxlength", "1000000")
 });
