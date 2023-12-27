6cc9f63df811f069a5fa01351c416c0c18b05c49
Matthew Lim <matthewlim230505@gmail.com>
2023-12-27 17:09:42 +0800

    Add support for another type of quiz

--- bing-scripts/quiz-answerer.js
+++ bing-scripts/quiz-answerer.js
@@ -8,6 +8,8 @@
 // @description Automatically chooses the right options for Bing Quizzes
 // ==/UserScript==
 
+
+// Quiz Type: Select 5 Answers
 // Get all elements with the class "b_cards bt_lstcl_card btcc btcNoImg"
 const elements = Array.from(document.querySelectorAll(".b_cards.bt_lstcl_card.btcc.btcNoImg"));
 
@@ -17,4 +19,27 @@ setTimeout(() => {
         console.log("Running");
         element.click();
     });
+}, 500);
+
+
+// Quiz Type: Select 1 Answer (Out of 4)
+setTimeout(() => {
+    // Get all elements with class "rqOption"
+    const elements = document.getElementsByClassName("rqOption");
+
+    // Iterate over the elements
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+
+        // Get the value of the element
+        const value = element.textContent.trim();
+
+        // Search for the value in the page's code
+        const searchResultCount = document.documentElement.innerHTML.split(value).length - 1;
+
+        // If there is more than one result, click on the button
+        if (searchResultCount > 1) {
+            element.click();
+        }
+    }
 }, 500);
