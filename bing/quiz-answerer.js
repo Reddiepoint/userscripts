224d60027791f3ada83570b26f77b989cc780c48
Reddiepoint <31847741+Reddiepoint@users.noreply.github.com>
2024-02-19 18:20:59 +0800

    Add partial support for This or That

--- bing/quiz-answerer.js
+++ bing/quiz-answerer.js
@@ -6,6 +6,7 @@
 // @version     2.0
 // @author      Reddiepoint
 // @description Automatically chooses the right options for Bing Quizzes
+// @run-at      document-idle
 // ==/UserScript==
 
 
@@ -31,6 +32,8 @@ function selectOneAnswer() {
     // Get all elements with class "rqOption"
     const elements = document.getElementsByClassName("rqOption");
 
+    // Get script content
+    const scriptContent = document.querySelector("body > script:nth-child(15)").textContent;
     // Iterate over the elements
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
@@ -38,12 +41,17 @@ function selectOneAnswer() {
         // Get the value of the element
         const value = element.textContent.trim();
 
-        // Search for the value in the page's code
-        const searchResultCount = document.documentElement.innerHTML.split(value).length - 1;
-
-        // If there is more than one result, click on the button
-        if (searchResultCount > 1) {
+        // // Search for the value in the page's code
+        // const searchResultCount = document.documentElement.innerHTML.split(value).length - 1;
+        //
+        // // If there is more than one result, click on the button
+        // if (searchResultCount > 1) {
+        //     element.click();
+        // }
+        // Check if the script contains the value of the element
+        if (scriptContent.includes(value)) {
             element.click();
+            // break; // Stop after clicking the correct answer
         }
     }
 }
@@ -52,32 +60,32 @@ setTimeout(selectOneAnswer, 500)
 
 
 // Quiz Type: This or That
-// function thisOrThat() {
-//     // Get all elements with class "btOptionText"
-//     const optionTextElements = document.getElementsByClassName("btOptionText");
-//
-//     // Iterate over the optionTextElements
-//     for (let i = 0; i < optionTextElements.length; i++) {
-//         const optionTextElement = optionTextElements[i];
-//
-//         // Get the text of the optionTextElement
-//         const optionText = optionTextElement.textContent.trim();
-//
-//         // Get the div with class "btOptionAns" inside the parent element
-//         const optionAnsElement = document.querySelector(".btOptionAns");
-//
-//         // Get the text of the optionAnsElement
-//         const optionAnsText = optionAnsElement.textContent.trim();
-//
-//         // Check if the optionAnsText contains the optionText
-//         if (optionAnsText.includes(optionText)) {
-//             // Get the parent element of the optionTextElement
-//             const parentElement = optionTextElement.parentElement;
-//
-//             // Click on the parent element
-//             parentElement.click();
-//         }
-//     }
-// }
-//
-// setTimeout(thisOrThat, 500);
+function thisOrThat() {
+    // Get all elements with class "btOptionText"
+    const optionTextElements = document.getElementsByClassName("btOptionText");
+    const questionText = document.querySelector("div.bt_queText");
+
+    // Form search
+    const search = questionText.textContent.trim() + " " + optionTextElements[0].textContent.trim() + " or " + optionTextElements[1].textContent.trim();
+
+    // Create button that copies search text to clipboard
+    const button = document.createElement("button");
+    button.innerText = "Copy Search";
+    button.addEventListener("click", () => {
+        // Copy search text to clipboard
+        navigator.clipboard.writeText(search);
+        button.innerText = "Copied!";
+        setTimeout(() => {
+            button.innerText = "Copy Search";
+        }, 2000);
+    });
+
+    // Insert button after question text
+    // Create a line break element
+    const lineBreak = document.createElement("br");
+    const pointsLocation = document.querySelector("#currentQuestionContainer > span > span.points");
+    pointsLocation.insertAdjacentElement("afterend", button);
+    pointsLocation.insertAdjacentElement("afterend", lineBreak);
+}
+
+setTimeout(thisOrThat, 500);
